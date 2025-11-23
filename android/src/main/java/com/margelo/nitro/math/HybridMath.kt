751e986700892b56c9ba7bd555d45b7941fb41ae
urm1n <urminhirpara@gmail.com>
2025-11-23 20:59:06 +0530

    feat(math): implement basic math addition for hybrid modules
    Add HybridMath implementation for both iOS and Android platforms
    Update nitro.json configuration to support autolinking

---
+++ android/src/main/java/com/margelo/nitro/math/HybridMath.kt
@@ -0,0 +1,9 @@
+package com.margelo.nitro.math
+
+import com.margelo.nitro.core.*
+
+class HybridMath : HybridMathSpec() {
+    override fun add(a: Double, b: Double): Double {
+        return a + b
+    }
+}
