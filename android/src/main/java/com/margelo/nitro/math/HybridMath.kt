f1b70783b8965851a012d7d98d7ee97a949a5bd2
urm1n <urminhirpara@gmail.com>
2025-11-23 21:27:13 +0530

    refactor(math): remove unused import in HybridMath class

--- android/src/main/java/com/margelo/nitro/math/HybridMath.kt
+++ android/src/main/java/com/margelo/nitro/math/HybridMath.kt
@@ -1,7 +1,5 @@
 package com.margelo.nitro.math
 
-import com.margelo.nitro.core.*
-
 class HybridMath : HybridMathSpec() {
     override fun add(a: Double, b: Double): Double {
         return a + b
